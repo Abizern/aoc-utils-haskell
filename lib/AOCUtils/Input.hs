cf8b116cb234cc58727943ab1477af774ecca6b6
Abizer Nasir <abizern@mac.com>
2023-11-27 12:29:38 +0000

    Restructure getRawInputs for use in the REPL

--- lib/AOCUtils/Input.hs
+++ lib/AOCUtils/Input.hs
@@ -1,21 +1,14 @@
-
 -- |
---    The module proovides helpers to load inputs for the solutions. It requires the inputs to be in ../inputs/<yyyy>/day<xx>.txt format.
+--    The module proovides helpers to load inputs for the solutions. It requires the inputs to be in ../inputs/day<xx>.txt format.
 --
 --    There are also helpers to extract them into common formats that can be further processed.
 module AOCUtils.Input
-  ( 
-    getRawInput,
+  ( getRawInput,
   )
 where
 
-import System.Environment (getArgs)
 import Text.Printf (printf)
 
-getRawInput :: Int -> Int -> IO String
-getRawInput y d = do
-  args <- getArgs
-  case args of
-    [] -> readFile (printf "inputs/%d/day%02d.txt" y d)
-    "+" : input : _ -> pure input
-    fp : _ -> readFile fp
+-- | This is a convenience function to load data for interactively running in the REPL
+getRawInput :: Int -> IO String
+getRawInput d = readFile (printf "inputs/day%02d.txt" d)
